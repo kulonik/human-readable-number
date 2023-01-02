a27af3796ea3b8814f55253454b82cea8cf84dac
kulonik <annakhmv@gmail.com>
2023-01-02 20:49:38 +0500

    feat: start solving the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    if (number === 1) {
+        return "one";
+    }
+    if (number === 2) {
+        return "two";
+    }
+    if (number === 3) {
+        return "three";
+    }
+    if (number === 4) {
+        return "four";
+    }
+    if (number === 5) {
+        return "five";
+    }
+    if (number === 6) {
+        return "six";
+    }
+    if (number === 7) {
+        return "seven";
+    }
+    if (number === 8) {
+        return "eight";
+    }
+    if (number === 9) {
+        return "nine";
+    }
+    if (number === 0) {
+        return "zero";
+    }
+    if (number === 10) {
+        return "ten";
+    }
+    if (number === 11) {
+        return "eleven";
+    }
+    if (number === 12) {
+        return "twelve";
+    }
+    if (number === 13) {
+        return "thirteen";
+    }
+    if (number === 14) {
+        return "fourteen";
+    }
+    if (number === 15) {
+        return "fifteen";
+    }
+    if (number === 16) {
+        return "sixteen";
+    }
+    if (number === 17) {
+        return "seventeen";
+    }
+    if (number === 18) {
+        return "eighteen";
+    }
+    if (number === 19) {
+        return "nineteen";
+    }
+};
